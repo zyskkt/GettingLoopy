61d7615b8933393e34b162a5bef54eeb78a439e4
Kent Zysk <kent@kentzysk.com>
2023-02-24 17:40:49 -0600

    Everything complete.

---
+++ src/CtoF.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class CtoF {
+    public static void main(String[] args) {
+        //Import scanner and initialize variables
+        Scanner in = new Scanner(System.in);
+        double tempF;
+        double tempC;
+        boolean validNumber = false;
+        String trash = "";
+        System.out.println("Enter temperature in Celcius:");
+        while (!validNumber) {
+            if (in.hasNextDouble()) {
+                //Get temp from the input
+                tempC = in.nextDouble();
+                in.nextLine(); // Clear buffer
+                //calculate celcius from farenheit input
+                tempF = (tempC * 1.8) + 32;
+                System.out.println(tempC + " Celcius equals " + tempF + " Farenheit.");
+            } else {
+                //Not a valid input, read as string instead
+                trash = in.nextLine();
+                //Let the user know input was invalid and to retry.
+                System.out.println("You entered an invalid temperature: " + trash);
+                System.out.println("Please try again with a valid number.");
+                System.out.println("Enter temperature in Celcius:");
+            }
+        }
+    }
+}
